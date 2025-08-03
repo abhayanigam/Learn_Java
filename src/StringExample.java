05d159e27b6ecd8a63ba2ede978aa5aa122a8b68
Abhaya Nigam <61977981+abhayanigam@users.noreply.github.com>
2025-08-03 23:30:04 +0530

    Update StringExample.java

--- src/StringExample.java
+++ src/StringExample.java
@@ -7,5 +7,21 @@ public class StringExample {
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
+
+        // iteration in String
+
+        String s4 = "java";
+
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
+            System.out.println(c);
+        }
+
+        for (char c : s1.toCharArray()) {
+            System.out.println(c);
+        }
+
+        s1.chars().forEach(ch -> System.out.println((char) ch));
+        
     }
 }
